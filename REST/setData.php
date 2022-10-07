ccdaa3d9ed7e3d009efd75543a645571ca9cc18b
MeridaNoe <105667207+MeridaNoe@users.noreply.github.com>
2022-10-07 10:22:17 -0500

    Add files via upload

--- REST/setData.php
+++ REST/setData.php
@@ -1,30 +1,28 @@
-<?php
-    ini_set('display_errors', 1);
-    $nombre = $_REQUEST["nombre"];
-    $apellido = $_REQUEST["apellido"];
-    $fdn = $_REQUEST["fdn"];
-    $salario = $_REQUEST["salario"];
-    $posiscion = $_REQUEST["posicion"];
-    $genero = $_REQUEST["genero"];
+<?php 
+ini_set('display_errors', 1);
+$nombre = $_REQUEST["nombre"];
+$apellido = $_REQUEST["apellido"];
+$fdn = $_REQUEST["fdn"];
+$salario = $_REQUEST["salario"];
+$posicion = $_REQUEST["posicion"];
+$genero = $_REQUEST["genero"];
 
-    if ($nombre != "" && $apellido != "" && $fdn != "" && $salario != "" 
-        && $posiscion != "" && $genero != ""
-    ) {
-        try {
-            $pdo = new PDO("mysql:host=localhost;dbname=staff", "root","");
-            $resultado = $pdo->prepare('INSERT INTO staff(fname,Iname,
-                                        position,sex,dob,salary)
-                                        VALUES(:a,:b,:c,:d,:e,:f)');
-            $resultado->bindParam(":a", $nombre, PDO::PARAM_STR);
-            $resultado->bindParam(":a", $apellido, PDO::PARAM_STR);
-            $resultado->bindParam(":a", $posiscion, PDO::PARAM_STR);
-            $resultado->bindParam(":a", $genero, PDO::PARAM_STR);
-            $resultado->bindParam(":a", $fdn, PDO::PARAM_STR);
-            $resultado->bindParam(":a", $salario, PDO::PARAM_STR);
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
-    }else{
-        echo "-1";
-    }
+if($nombre != "" && $apellido != "" && $fdn != "" && $salario != "" && $posicion != "" && $genero != ""){
+    try{
+        $pdo = new PDO("mysql:host=localhost;dbname=staff", "root", "");
+        $resultado = $pdo->prepare('INSERT INTO staff(fname, Iname,position, sex, dob, salary) VALUES (:a, :b, :c, :d, :e, :f)');
+        $resultado->bindParam(":a", $nombre, PDO::PARAM_STR);
+        $resultado->bindParam(":b", $apellido, PDO::PARAM_STR);
+        $resultado->bindParam(":c", $posicion, PDO::PARAM_STR);
+        $resultado->bindParam(":d", $genero, PDO::PARAM_STR);
+        $resultado->bindParam(":e", $fdn, PDO::PARAM_STR);
+        $resultado->bindParam(":f", $salario, PDO::PARAM_INT);
+        $resultado->execute();
+        printf("aaaaaaaaaaa");
+    } catch (PDOException $e) {
+        var_dump($e);
+    }    
+} else {
+    echo "-1";
+}   
 ?>
